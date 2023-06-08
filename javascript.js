aec48eb698d8ab3393d9b97e887a6a3a97a74922
paulshadowsx <richard_scream@hotmail.com>
2023-06-08 03:53:08 -0600

    Add playRound function

--- javascript.js
+++ javascript.js
@@ -8,3 +8,24 @@ function getComputerChoice() {
         return "scissors";
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+     if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You Win! Rock beats Scissors";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You Win! Paper beats Rock";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You Win! Scissors beats Paper";
+    } else if (computerSelection === "rock" && playerSelection === "scissors") {
+        return "You Lose! Rock beats Paper";
+    } else if (computerSelection === "paper" && playerSelection === "rock") {
+        return "You Lose! Paper beats Rock";
+    } else if (computerSelection === "scissors" && playerSelection === "paper") {
+        return "You Lose! Scissors beats Paper";
+    } else {
+        return "Draw!";
+    }
+}
+
+const playerSelection = prompt("Make your Choice!\nHint: Rock, Paper or Scissors").toLowerCase();
+const computerSelection = getComputerChoice();
